e7895a309ea07649a55c0dff3b068c4d141e46ef
unknown <if.garcia11@uniandes.edu.co>
2016-02-09 12:25:42 -0500

    Busco Ayuda url update

--- BuscoAyudaGrupo2/urls.py
+++ BuscoAyudaGrupo2/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.conf.urls import include, url
 import polls.urls
 urlpatterns = [
-    url(r'^polls/admin/', admin.site.urls),
-    url(r'^polls/', include(polls.urls, namespace='trabajador')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$/', include(polls.urls, namespace='trabajador')),
 ]
