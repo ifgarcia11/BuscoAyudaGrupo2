d6b6dbcfba2abc87d1ab8092424604ffdfa7a50e
unknown <if.garcia11@uniandes.edu.co>
2016-02-09 16:25:38 -0500

    postgreSql

--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,12 @@ from .models import trabajador
 from .models import tiposDeServicio
 
 # Create your views here.
-def index (request):
-    trabajadores = trabajador.objects.all()
-    context = {'trabajadores':trabajadores}
-    return  render(request, 'polls/templates/index.html', context)
 
 def servicios (request):
     tiposDeServicios = tiposDeServicio.objects.all()
     context = {'tiposDeServicios':tiposDeServicios}
     return  render(request, 'polls/templates/tiposDeServicios.html', context)
+
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
