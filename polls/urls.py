f360a68caccc8a2c7261c8ae9384ad7094ebac1f
unknown <if.garcia11@uniandes.edu.co>
2016-02-09 12:47:05 -0500

    index

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns,url
 from polls import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
         url(r'^tiposDeServicios/$', views.servicios, name='tiposDeServicios')
 
 
