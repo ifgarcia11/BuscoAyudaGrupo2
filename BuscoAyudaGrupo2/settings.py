e056a74cacde1452bc34565c7a50386448d4adfe
unknown <if.garcia11@uniandes.edu.co>
2016-02-09 15:59:44 -0500

    config files

--- BuscoAyudaGrupo2/settings.py
+++ BuscoAyudaGrupo2/settings.py
@@ -123,4 +123,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT =os.path.join(PROJECT_ROOT,'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
