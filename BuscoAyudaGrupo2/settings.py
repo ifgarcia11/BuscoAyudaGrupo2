ef8eff252f4cba95b3943ddfb4b78cb6e3c58bac
unknown <if.garcia11@uniandes.edu.co>
2016-02-09 12:22:38 -0500

    Remove Static urls

--- BuscoAyudaGrupo2/settings.py
+++ BuscoAyudaGrupo2/settings.py
@@ -119,12 +119,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'polls/Templates/polls')
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static/web'),
-    os.path.join(PROJECT_ROOT, 'static/web/images'),
-)
